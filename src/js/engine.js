886523f357f9d9d5b59fbba3b3357be5efe7fafa
netosarmento <netosarmento@gmail.com>
2024-10-26 22:08:35 -0300

    ajeitando tudo

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,34 +1,35 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key"); // escolhendo as classes que quero 
-
-const volumeSlider = document.querySelector(".volume-slider input"); // ajeitando o volume
-
-let mapedkeys = [];
-
-let audio = new Audio(`/src/tunes/a.wav`); 
-
-
-
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+let mapedkeys = []; // biblioteca das teclas mapeadas
+let audio = new Audio(`/src/tunes/a.wav`);
 
 const playTune = (key) => {
-    audio.src = `/src/tunes/${key}.wav`;  // usando variavel
+    audio.src = `/src/tunes/${key}.wav`;  // Usando a variável para mudar a fonte do áudio
     audio.play();
-    
+
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
-    pianoKeys.forEach((key) => {
-    key.addEventListener("click", () => playTune(key.dataset.key));
-    clickedKey.classList.add("active"); // importando a classe active tive que importa como classe
+    clickedKey.classList.add("active"); // Adiciona a classe active
     setTimeout(() => {
-        clickedKey.classList.remove("active");
+        clickedKey.classList.remove("active"); // Remove a classe active após 150ms
     }, 150);
+};
+
+// Adicionando o evento de clique para cada tecla
+pianoKeys.forEach((key) => {
+    key.addEventListener("click", () => {
+        playTune(key.dataset.key);
+    });
 });
-}
-document.addEventListener("keydown", (e) => {
+
+document.addEventListener("keydown", (e) => { // colocando para fazer som ao apertar a tecla
     if (mapedkeys.includes(e.key)) {
         playTune(e.key);
     }
-})
-const handleVolume = (e)=> {
-    audio.volume = e.target.value;
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value; // Atualiza o volume do áudio
+    
 };
 
-volumeSlider.addEventListener("input", handleVolume); //chamando a função
+volumeSlider.addEventListener("input", handleVolume); // Chamando a função
